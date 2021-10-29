01f41fee3eafae2200c0bac82f9e17ba9398174a
Vashishth Patel <vashishthchaudhary48@gmail.com>
2021-10-29 21:27:03 +0530

    carosol added with images

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	<link rel="icon" href="assets/img/logo.png">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<title>Labor For Rural</title>
-
+	<link rel="stylesheet" href="assets/css/style.css">
 
 </head>
 <body>
@@ -47,11 +47,106 @@
 	</nav>
 	<br>
 
+	<!-- filters -->
+	<div class="container">
+		<input class="form-control" id="myInput" type="text" placeholder="Search..">
+	</div>
+<!-- work section -->
+	
+	<div class="container myTable">
+		<div class="container mt-5 mb-3">
+		    <div class="row">
+		        <div class="col-md-4 rr">
+		            <div class="card p-3 mb-2">
+		                <div class="d-flex justify-content-between">
+		                    <div class="d-flex flex-row align-items-center">
+		                        <div class="icon"> <img class="avatar11" src="https://www.w3schools.com/w3images/avatar6.png" alt="avatar" /> </div>
+		                        <div class="ms-2 c-details">
+		                            <h6 class="mb-0">Rameshbhai</h6> <span>1 days ago</span>
+		                        </div>
+		                    </div>
+		                </div><br>
+		                <div class="d-flex">
+		                    <div class="badge"> <span>Design</span> </div>
+		                    <div class="badge"> <span>Design</span> </div>
+		                </div>
+		                <div class="mt-4">
+		                    <h4 class="heading">Wheat Cutting</h4>
+		                    <h5>250 ₹</h5>
+		                    <h6><span class="text2">Timings : </span> 8 AM to 12 PM</h6>
+		                    <div class="mt-5">
+		                        <div class="mt-3"> <span class="text1">32 Applied <span class="text2">of 50 capacity</span></span> </div>
+		                    </div>
+		                </div>
+		            </div>
+		        </div>
+		        <div class="col-md-4 rr">
+		            <div class="card p-3 mb-2">
+		                <div class="d-flex justify-content-between">
+		                    <div class="d-flex flex-row align-items-center">
+		                        <div class="icon"> <img class="avatar11" src="https://www.w3schools.com/w3images/avatar6.png" alt="avatar" /> </div>
+		                        <div class="ms-2 c-details">
+		                            <h6 class="mb-0">Rameshbhai</h6> <span>1 days ago</span>
+		                        </div>
+		                    </div>
+		                </div><br>
+		                <div class="d-flex">
+		                    <div class="badge"> <span>Design</span> </div>
+		                    <div class="badge"> <span>Design</span> </div>
+		                </div>
+		                <div class="mt-4">
+		                    <h4 class="heading">Wheat Cutting</h4>
+		                    <h5>250 ₹</h5>
+		                    <h6><span class="text2">Timings : </span> 8 AM to 12 PM</h6>
+		                    <div class="mt-5">
+		                        <div class="mt-3"> <span class="text1">32 Applied <span class="text2">of 50 capacity</span></span> </div>
+		                    </div>
+		                </div>
+		            </div>
+		        </div>
+		        <div class="col-md-4 rr">
+		            <div class="card p-3 mb-2">
+		                <div class="d-flex justify-content-between">
+		                    <div class="d-flex flex-row align-items-center">
+		                        <div class="icon"> <img class="avatar11" src="https://www.w3schools.com/w3images/avatar6.png" alt="avatar" /> </div>
+		                        <div class="ms-2 c-details">
+		                            <h6 class="mb-0">Rameshbhaixx</h6> <span>1 days ago</span>
+		                        </div>
+		                    </div>
+		                </div><br>
+		                <div class="d-flex">
+		                    <div class="badge"> <span>Design</span> </div>
+		                    <div class="badge"> <span>Design</span> </div>
+		                </div>
+		                <div class="mt-4">
+		                    <h4 class="heading">Wheat Cutting</h4>
+		                    <h5>250 ₹</h5>
+		                    <h6><span class="text2">Timings : </span> 8 AM to 12 PM</h6>
+		                    <div class="mt-5">
+		                        <div class="mt-3"> <span class="text1">32 Applied <span class="text2">of 50 capacity</span></span> </div>
+		                    </div>
+		                </div>
+		            </div>
+		        </div>	
+		    </div>
+		</div>
+	</div>
+
+
 
 
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
+	<script>
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $(".myTable .rr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+</script>
 </body>
 </html>
