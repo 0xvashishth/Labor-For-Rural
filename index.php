e247b2cadec327c3474ba29e9f917d8fb370b496
Vashishth Patel <vashishthchaudhary48@gmail.com>
2021-10-30 00:24:17 +0530

    new created files and button added

--- index.php
+++ index.php
@@ -39,10 +39,8 @@
 	          <!-- <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Disabled</a> -->
 	        </li>
 	      </ul>
-	      <form class="d-flex">
-	        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-	        <button class="btn btn-outline-primary" type="submit">Search</button>
-	      </form>
+	        <a class="btn btn-outline-primary" href="login.php">Login</a> &nbsp;
+	        <a class="btn btn-outline-secondary" href="login.php">Register</a>
 	    </div>
 	  </div>
 	</nav>
