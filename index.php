d4ced374c9f3ab5e9fd24768ae201d8388e6d7e5
Vashishth Patel <vashishthchaudhary48@gmail.com>
2021-10-29 21:25:44 +0530

    Update index.php

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="assets/img/logo.png">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<title>Labor For Rural</title>
+
+
+</head>
+<body>
+
+	<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+	  <div class="container-fluid">
+	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+	      <span class="navbar-toggler-icon"></span>
+	    </button>
+	    <div class="navbar-toggler">
+	    	<img src="assets/img/logo.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
+	    	<b>Labor For Rural</b>
+	    </div>
+	    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+	      <a class="navbar-brand d-sm-none d-md-block d-none d-sm-block d-md-none d-lg-block" href="#">
+	      	<img src="assets/img/logo.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
+			<b>Labor For Rural</b>
+		  </a>
+	      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+	        <li class="nav-item">
+	          <a class="nav-link active" aria-current="page" href="#">Work</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link active" href="#">Add Work</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link active" href="#">Contact-us</a>
+	          <!-- <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Disabled</a> -->
+	        </li>
+	      </ul>
+	      <form class="d-flex">
+	        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+	        <button class="btn btn-outline-primary" type="submit">Search</button>
+	      </form>
+	    </div>
+	  </div>
+	</nav>
+	<br>
+
+
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
