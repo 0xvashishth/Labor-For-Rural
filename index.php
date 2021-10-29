8b323098557af9cb517936b5a5af0ae710e58327
Vashishth Patel <vashishthchaudhary48@gmail.com>
2021-10-29 21:28:36 +0530

    index file updated

--- index.php
+++ index.php
@@ -132,7 +132,46 @@
 		</div>
 	</div>
 
-
+	<div class="container">
+		  <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+		  <div class="carousel-indicators">
+		    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+		    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+		    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+		  </div>
+		  <div class="carousel-inner">
+		    <div class="carousel-item active">
+		      <img src="assets/img/labor1.jpg" class="d-block w-100" height="600px" alt="...">
+		      <div class="carousel-caption d-none d-md-block">
+		        <h1>First slide label</h1>
+		        <h3>Some representative placeholder content for the first slide.</h3>
+		      </div>
+		    </div>
+		    <div class="carousel-item">
+		      <img src="assets/img/laborone2.jpg" class="d-block w-100" height="600px" alt="...">
+		      <div class="carousel-caption d-none d-md-block">
+		        <h1>First slide label</h1>
+		        <h3>Some representative placeholder content for the first slide.</h3>
+		      </div>
+		    </div>
+		    <div class="carousel-item">
+		      <img src="assets/img/labor3.jpg" class="d-block w-100" height="600px" alt="...">
+		      <div class="carousel-caption d-none d-md-block">
+		        <h1>First slide label</h1>
+		        <h3>Some representative placeholder content for the first slide.</h3>
+		      </div>
+		    </div>
+		  </div>
+		  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+		    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		    <span class="visually-hidden">Previous</span>
+		  </button>
+		  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+		    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+		    <span class="visually-hidden">Next</span>
+		  </button>
+		</div>
+	</div>
 
 
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
