831fc59935fc907c7749ed328a3257f53b125e7d
Vashishth Patel <vashishthchaudhary48@gmail.com>
2021-10-30 12:23:45 +0530

    Create error.php

---
+++ profiles/error.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <title>Labor For Rural</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+<div class="container"><br><br>
+  <center><a href="index.php" class="brand-logo">
+      <img src="img/logo.png" width="300">
+    </a></center>
+<div class="notfound-404">
+<h1>404</h1>
+</div>
+<h2>Oops! This Page Could Not Be Found</h2>
+<p>Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable</p>
+<a href="#">Go To Homepage</a>
+</div>
+</body>
+</html>
