b4314ddd32c667fef7b9cbeaa53f212ae7802b09
Vashishth Patel <vashishthchaudhary48@gmail.com>
2021-10-30 14:07:08 +0530

    filters removed

--- profiles/main/index.php
+++ profiles/main/index.php
@@ -63,32 +63,6 @@ else{
 	<!-- filters -->
 	<div class="container ">
 			<input class="form-control" id="myInput" type="text" placeholder="Search by keyword...">
-			<br>
-			<div class="row d-flex justify-content-center">
-				<select class="form-select w-25" id="cropselect" aria-label="Default select example">
-				  <option selected>Filter by Crop</option>
-				  <option value="rice">rice</option>
-				  <option value="wheat">wheat</option>
-				  <option value="tobacco">tobacco</option>
-				  <option value="sugarcane">sugarcane</option>
-				</select>
-				&nbsp;&nbsp;
-				<select class="form-select w-25" id="timingselect" aria-label="Default select example">
-				  <option selected>Filter by Timing</option>
-				  <option value="rice">rice</option>
-				  <option value="wheat">wheat</option>
-				  <option value="tobacco">tobacco</option>
-				  <option value="sugarcane">sugarcane</option>
-				</select>
-				&nbsp;&nbsp;
-				<select class="form-select w-25" id="workselect" aria-label="Default select example">
-				  <option selected>Filter by work</option>
-				  <option value="harvesting">harvesting</option>
-				  <option value="planting">planting</option>
-				  <option value="plough-fields">plough fields</option>
-				  <option value="other">other</option>
-				</select>
-			</div>
 	</div>
 <!-- work section -->
 	
@@ -100,7 +74,7 @@ else{
 
 						include '../config.php';
 
-						$sql = "SELECT * FROM `work`";
+						$sql = "SELECT * FROM `work` order by `created_time` DESC LIMIT 100";
 						$result = mysqli_query( $conn , $sql );
 						if( !$result )
 							die( "Something went wrong" );
